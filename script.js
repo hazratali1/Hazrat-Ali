c77aaad56746aabbbb8dbfe5cd971634cc6d76c9
Hazrat Ali <163515344+hazratali1@users.noreply.github.com>
2026-01-27 07:28:01 +0600

    Implement mobile navigation toggle
    Added mobile navigation toggle functionality.

--- script.js
+++ script.js
@@ -139,3 +139,15 @@ function loadProjects(category) {
     projectCards.appendChild(card);
   });
 }
+
+/* ======================
+   MOBILE NAV TOGGLE
+====================== */
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
